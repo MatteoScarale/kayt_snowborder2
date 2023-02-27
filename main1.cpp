6b6e98d66e52df37677fa4ec4eeed0bef5dee4ab
MatteoScarale <122804458+MatteoScarale@users.noreply.github.com>
2023-02-27 17:07:34 +0100

    Update main1.cpp

--- main1.cpp
+++ main1.cpp
@@ -1,3 +1,13 @@
+/**
+ * @file main1.cpp
+ * @author matteo scarale (matteoscaralej@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-02-27
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -8,24 +18,39 @@
 
 using namespace std;
 
+/**
+Struttura che rappresenta un partecipante
+*/
 struct partecipante
 {
-  string nome;
-  string cognome;
-  int matricola;
-  double x1;
-  double y1;
-  double x2;
-  double y2;
-  double distanza;
+string nome; ///< Nome del partecipante
+string cognome; ///< Cognome del partecipante
+int matricola; ///< Numero di matricola del partecipante
+double x1; ///< Coordinata x del punto 1
+double y1; ///< Coordinata y del punto 1
+double x2; ///< Coordinata x del punto 2
+double y2; ///< Coordinata y del punto 2
+double distanza; ///< Distanza tra i due punti
 };
+/**
 
+Funzione che confronta due partecipanti in base alla distanza
+@param p1 primo partecipante da confrontare
+@param p2 secondo partecipante da confrontare
+@return true se la distanza del primo partecipante è maggiore della distanza del secondo partecipante, false altrimenti
+*/
 bool
 confrontaPartecipanti (const partecipante & p1, const partecipante & p2)
 {
   return p1.distanza > p2.distanza;
 }
+/**
 
+Funzione che confronta due partecipanti in base alla distanza per la scrittura su file
+@param p1 primo partecipante da confrontare
+@param p2 secondo partecipante da confrontare
+@return true se la distanza del primo partecipante è maggiore della distanza del secondo partecipante, false altrimenti
+*/
 bool
 confrontaPartecipantiFile (const partecipante & p1, const partecipante & p2)
 {
@@ -72,10 +97,10 @@ main ()
 // Funzione che calcola la distanza euclidea tra due punti
       partecipanti[i].distanza =
 	sqrt (pow (partecipanti[i].x1 - partecipanti[i].x2, 2) +
-	      pow (partecipanti[i].y1 - partecipanti[i].y2, 2));
+	      pow (partecipanti[i].y1 - partecipanti[i].y2, 2));//formula per calcolare la distanza tra due punti
 
       sort (partecipanti, partecipanti + 4, confrontaPartecipanti);
-      sort (partecipanti, partecipanti + 4, confrontaPartecipantiFile);
+      sort (partecipanti, partecipanti + 4, confrontaPartecipantiFile);//funzione cghe mette in classifica i partecipanti
       cout << "Classifica in base alla distanza percorsa:" << endl;
       outFile << "Classifica in base alla distanza percorsa: " << endl;
       for (int i = 0; i < 4; i++)
@@ -94,7 +119,16 @@ main ()
 	    matricola << "): " << partecipanti[i].
 	    distanza << " metri" << endl;
 	}
+/**
 
+@brief Chiude il file di output e legge i record dal file di input
+
+La funzione chiude il file di output e legge i record dal file di input "partecipanti.txt",
+
+inserendoli in un array di oggetti partecipante.
+
+@return 1 se non è possibile aprire il file di input, altrimenti non restituisce nulla.
+*/
       outFile.close ();		// Chiude il file
 
       // Legge i record dal file e li mette in un array
